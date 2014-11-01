b29bf82d4104ff5930d2c46fbcac3285edac5bbf
Sudo Systems <bram@sudo-systems.com>
2014-11-01 22:22:54 +0100

    - Restructured code to be easier to work with

--- demo/scripts/jquery.sudo-notify.js
+++ demo/scripts/jquery.sudo-notify.js
@@ -1,3 +1,17 @@
+String.prototype.isIn = function() {
+  if(this === 'undefined') {
+    return false;
+  }
+  
+  for(var i=0; i<arguments.length; i++) {
+    if($.trim(arguments[i]) === $.trim(this.toString())){ 
+      return true;
+    }
+  }
+  
+  return false;
+};
+
 (function($) {
   $.fn.sudoNotify = function(options) {
     var settings = $.extend(true, {}, $.fn.sudoNotify.defaults, options);
@@ -6,52 +20,31 @@
     var timer = null;
     var element = this;
     var currentMessageType = '';
-    
-    //Compose DOM elements and append to container element
-    var cssPosition = this.parent().is('body')? 'fixed' : 'absolute';
-    var messageContainer = $('<div></div>').addClass('message');
-    var closeButtonContainer = $('<div></div>').addClass('close-button');
-    var wrapper = $('<div></div>').addClass('wrapper', 'sn-clearfix').css(settings.defaultStyle).append(messageContainer, closeButtonContainer);
-    this.addClass('sudoNotify', 'sn-clearfix').css('position', cssPosition).append(wrapper);
-    closeButtonContainer.toggle(settings.showCloseButton);
-    
-    //Prepare parent element if required
-    if(!this.parent().is('body')) {
-      if(this.parent().css('position') === 'undefined' || $.trim(this.parent().css('position')) === '' || $.trim(this.parent().css('position')) === 'static') {
-        this.parent().css('position', 'relative');
-      }
+    var cssPosition, messageContainer, closeButtonContainer, wrapper;
+
+    init();
+
+    //Private methods
+    function init() {
+      cssPosition = element.parent().is('body')? 'fixed' : 'absolute';
+      messageContainer = $('<div></div>').addClass('message');
+      closeButtonContainer = $('<div></div>').addClass('close-button');
+      wrapper = $('<div></div>').addClass('wrapper', 'sn-clearfix').css(settings.defaultStyle).append(messageContainer, closeButtonContainer);
       
-      if(this.parent().css('overflow') === 'undefined' || $.trim(this.parent().css('overflow')) === '' || $.trim(this.parent().css('overflow')) === 'visible') {
-        this.parent().css('overflow', 'hidden');
+      element.addClass('sudoNotify', 'sn-clearfix').css('position', cssPosition).append(wrapper);
+      closeButtonContainer.toggle(settings.showCloseButton);
+
+      if(!element.parent().is('body')) {
+        if(element.parent().css('position').isIn('', 'static')){
+          element.parent().css('position', 'relative');
+        }
+
+        if(element.parent().css('overflow').isIn('', 'visible')){
+          element.parent().css('overflow', 'hidden');
+        }
       }
     }
 
-    //Public methods
-    this.error = function(message) {
-      show('error', message);
-    };
-    
-    this.warning = function(message) {
-      show('warning', message);
-    };
-    
-    this.success = function(message) {
-      show('success', message);
-    };
-    
-    this.setOption = function(key, value) {
-      settings[key] = value;
-    };
-    
-    this.getOption = function(key) {
-      return settings[key];
-    };
-    
-    this.close = function() {
-      executeHide();
-    };
-    
-    //Private methods
     function setClass(className) {
       element.removeClass('error warning success').addClass(className);
     }
@@ -81,80 +74,23 @@
       callback = (callback === 'undefined' || typeof callback !== 'function')? function(){} : callback;
       clearTimeout(timer);
       setClass(messageType);
-      
-      if(messageType === 'error') {
-        element.css(settings.errorStyle);
-        messageContainer.html(message);
-      }
-      else if(messageType === 'warning') {
-        element.css(settings.warningStyle);
-        messageContainer.html(message);
-      }
-      else if (messageType === 'success') {
-        element.css(settings.successStyle);
-        messageContainer.html(message);
-      }
-      
-      if(settings.animation.type === 'scroll-right' || settings.animation.type === 'scroll-left' || settings.animation.type === 'scroll-right-fade' || settings.animation.type === 'scroll-left-fade') {
-        var initialPosition = (settings.animation.type === 'scroll-right' || settings.animation.type === 'scroll-right-fade')? '-'+element.width()+'px' : element.width()+'px';
-        
-        if(settings.animation.type === 'scroll-right-fade' || settings.animation.type === 'scroll-left-fade') {
-          element.css('opacity', 0.0);
-        }
-        
-        wrapper.css('opacity', 0.0);
-        element.css('left', initialPosition).show();
-        var animationOptions = {left: '0px'};
-        
-        if(settings.animation.type === 'scroll-right-fade' || settings.animation.type === 'scroll-left-fade') {
-          animationOptions.opacity= settings.opacity;
-        }
+      element.css(settings[messageType+'Style']);
+      messageContainer.html(message);
 
-        element.stop().animate(animationOptions, parseInt(settings.animation.showSpeed), 
-          function() {
-            settings.onShow(currentMessageType);
-            callback();
-            wrapper.stop().animate({opacity:1.0}, (parseInt(settings.animation.showSpeed)/3));
-          }
-        );
+      if(settings.animation.type.isIn('scroll-right', 'scroll-left', 'scroll-right-fade', 'scroll-left-fade')) {
+        scrollIn(callback);
       }
-      else if(settings.animation.type === 'slide' || settings.animation.type === 'slide-fade') {
-        if(settings.animation.type === 'slide-fade') {
-          element.css('opacity', 0.0);
-        }
-        
-        wrapper.css('opacity', 0.0);
-        element.css(settings.position, '-'+element.height()).show();
-        
-        var animationOptions = {};
-        animationOptions[settings.position] = '0px';
-        
-        if(settings.animation.type === 'slide-fade') {
-          animationOptions['opacity'] = settings.opacity;
-        }
-        
-        element.stop().animate(animationOptions, parseInt(settings.animation.showSpeed), 
-          function() {
-            settings.onShow(currentMessageType);
-            callback();
-            wrapper.stop().animate({opacity:1.0}, (parseInt(settings.animation.showSpeed)/3));
-          }
-        );
+      else if(settings.animation.type.isIn('slide', 'slide-fade') ) {
+        slideIn(callback);
       }
       else if(settings.animation.type === 'fade') {
-        element.fadeIn(parseInt(settings.animation.showSpeed), 
-          function() {
-            settings.onShow(currentMessageType);
-            callback();
-          }
-        );
+        fadeIn(callback);
       }
       else {
-        element.show();
-        settings.onShow(currentMessageType);
+        simpleShow(callback);
       }
 
-      if(settings.log) {
+      if(settings.log === true) {
         debug(messageType, message);
       }
     }
@@ -163,53 +99,17 @@
       callback = (callback === 'undefined' || typeof callback !== 'function')? function(){} : callback;
       clearTimeout(timer);
       
-      if(settings.animation.type === 'scroll-right' || settings.animation.type === 'scroll-left' || settings.animation.type === 'scroll-right-fade' || settings.animation.type === 'scroll-left-fade') {
-        var targetPosition = (settings.animation.type === 'scroll-right' || settings.animation.type === 'scroll-right-fade')? '-'+element.width()+'px' : element.width()+'px';
-        var animationOptions = {left: targetPosition};
-        
-        if(settings.animation.type === 'scroll-right-fade' || settings.animation.type === 'scroll-left-fade') {
-          animationOptions.opacity= 0.0;
-        }
-
-        wrapper.stop().animate({opacity:0.0}, (parseInt(settings.animation.hideSpeed)/2));
-        element.stop().animate(animationOptions, parseInt(settings.animation.hideSpeed), 
-          function(){
-            element.hide();
-            settings.onClose(currentMessageType);
-            callback();
-          }
-        );
+      if(settings.animation.type.isIn('scroll-right', 'scroll-left', 'scroll-right-fade', 'scroll-left-fade')) {
+        scrollOut(callback);
       }
-      else if(settings.animation.type === 'slide' || settings.animation.type === 'slide-fade') {
-        var animationOptions = {};
-        animationOptions[settings.position] = '-'+element.height();
-        
-        if(settings.animation.type === 'slide-fade') {
-          animationOptions['opacity'] = 0.0;
-        }
-
-        wrapper.stop().animate({opacity:0.0}, (parseInt(settings.animation.hideSpeed)/2));
-        element.stop().animate(animationOptions, parseInt(settings.animation.hideSpeed), 
-          function(){
-            element.hide();
-            settings.onClose(currentMessageType);
-            callback();
-          }
-        );
+      else if(settings.animation.type.isIn('slide', 'slide-fade')) {
+        slideOut(callback);
       }
       else if(settings.animation.type === 'fade') {
-        element.fadeOut(parseInt(settings.animation.hideSpeed),
-          function() {
-            settings.onClose(currentMessageType);
-            callback();
-            currentMessageType = '';
-          }
-        );
+        fadeOut(callback);
       }
       else {
-        element.hide();
-        settings.onClose(currentMessageType)
-        currentMessageType = '';
+        simpleHide(callback);
       }
     }
     
@@ -220,6 +120,123 @@
           executeHide();
         }, (parseInt(settings.duration)*1000));
       }
+    }
+    
+    //Animations
+    function scrollIn(callback) {
+      var initialPosition = settings.animation.type.isIn('scroll-right', 'scroll-right-fade')? '-'+element.width()+'px' : element.width()+'px';
+        
+      if(settings.animation.type.isIn('scroll-right-fade', 'scroll-left-fade')) {
+        element.css('opacity', 0.0);
+      }
+
+      wrapper.css('opacity', 0.0);
+      element.css('left', initialPosition).show();
+      var animationOptions = {left: '0px'};
+
+      if(settings.animation.type.isIn('scroll-right-fade', 'scroll-left-fade')) {
+        animationOptions.opacity= settings.opacity;
+      }
+
+      element.stop().animate(animationOptions, parseInt(settings.animation.showSpeed), 
+        function() {
+          settings.onShow(currentMessageType);
+          callback();
+          wrapper.stop().animate({opacity:1.0}, (parseInt(settings.animation.showSpeed)/3));
+        }
+      );
+    }
+    
+    function scrollOut(callback) {
+      var targetPosition = settings.animation.type.isIn('scroll-right', 'scroll-right-fade')? '-'+element.width()+'px' : element.width()+'px';
+      var animationOptions = {left: targetPosition};
+
+      if(settings.animation.type.isIn('scroll-right-fade', 'scroll-left-fade')) {
+        animationOptions.opacity= 0.0;
+      }
+
+      wrapper.stop().animate({opacity:0.0}, (parseInt(settings.animation.hideSpeed)/2));
+      element.stop().animate(animationOptions, parseInt(settings.animation.hideSpeed), 
+        function(){
+          element.hide();
+          settings.onClose(currentMessageType);
+          callback();
+        }
+      );
+    }
+    
+    function slideIn(callback) {
+      if(settings.animation.type === 'slide-fade') {
+        element.css('opacity', 0.0);
+      }
+
+      wrapper.css('opacity', 0.0);
+      element.css(settings.position, '-'+element.height()).show();
+
+      var animationOptions = {};
+      animationOptions[settings.position] = '0px';
+
+      if(settings.animation.type === 'slide-fade') {
+        animationOptions['opacity'] = settings.opacity;
+      }
+
+      element.stop().animate(animationOptions, parseInt(settings.animation.showSpeed), 
+        function() {
+          settings.onShow(currentMessageType);
+          callback();
+          wrapper.stop().animate({opacity:1.0}, (parseInt(settings.animation.showSpeed)/3));
+        }
+      );
+    }
+    
+    function slideOut(callback) {
+      var animationOptions = {};
+      animationOptions[settings.position] = '-'+element.height();
+
+      if(settings.animation.type === 'slide-fade') {
+        animationOptions['opacity'] = 0.0;
+      }
+
+      wrapper.stop().animate({opacity:0.0}, (parseInt(settings.animation.hideSpeed)/2));
+      element.stop().animate(animationOptions, parseInt(settings.animation.hideSpeed), 
+        function(){
+          element.hide();
+          settings.onClose(currentMessageType);
+          callback();
+        }
+      );
+    }
+    
+    function fadeIn(callback) {
+      element.fadeIn(parseInt(settings.animation.showSpeed), 
+        function() {
+          settings.onShow(currentMessageType);
+          callback();
+        }
+      );
+    }
+    
+    function fadeOut(callback) {
+      element.fadeOut(parseInt(settings.animation.hideSpeed),
+        function() {
+          settings.onClose(currentMessageType);
+          callback();
+          currentMessageType = '';
+        }
+      );
+    }
+    
+    function simpleShow(callback) {
+      element.show();
+      callback();
+      settings.onShow(currentMessageType);
+    }
+    
+    function simpleHide(callback) {
+      element.hide();
+      callback();
+      settings.onClose(currentMessageType);
+      currentMessageType = '';
     }
     
     closeButtonContainer.on('click', function(e) {
@@ -251,7 +268,32 @@
         window.console.log(getDateTime()+' ~ ' +messageType+ ': "' +data+ '"');
       }
     };
+
+    //Public methods
+    this.error = function(message) {
+      show('error', message);
+    };
     
+    this.warning = function(message) {
+      show('warning', message);
+    };
+    
+    this.success = function(message) {
+      show('success', message);
+    };
+    
+    this.setOption = function(key, value) {
+      settings[key] = value;
+    };
+    
+    this.getOption = function(key) {
+      return settings[key];
+    };
+    
+    this.close = function() {
+      executeHide();
+    };
+
     return this;
   };
   
