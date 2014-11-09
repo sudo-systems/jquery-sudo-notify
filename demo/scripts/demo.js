19b3d218871637b1a6bce5a92a6c27e69f92ecb3
Jasper <jasper.heidinga@rtl.nl>
2014-11-09 17:51:54 +0100

    Plugin rework

--- demo/scripts/demo.js
+++ demo/scripts/demo.js
@@ -1,146 +1,143 @@
-$(document).ready(function(){
-  var showCallbackNotifications = false;
-  var countdown = null;
+      $(document).ready(function(){
+        var showCallbackNotifications = false;
+        var countdown = null;
+        
+        $('.notification-container').sudoNotify({
+          autoHide: ($('#timeout').val() !== '0'),
+          showCloseButton: ($('#showCloseButton').val() === '1'),
+          duration: $('#timeout').val(), //seconds
+          position: $('#position').val(), //top or bottom
+          log: true,
+          opacity: 0.95,
+          defaultStyle: {
+            maxWidth: '1000px',
+            fontSize: '16px'
+          },
+          errorStyle: {
+            color: '#000000',
+            backgroundColor: '#FF9494'
+          },
+          warningStyle: {
+            color: '#000000',
+            backgroundColor: '#FFFF96'
+          },
+          successStyle: {
+            color: '#000000',
+            backgroundColor: '#B8FF6D'
+          },
+          animation: {
+            type: $('#animationType').val(), //fade, scroll-left, scroll-left-fade, scroll-right, scroll-right-fade, slide, slide-fade or none
+            showSpeed: 400,
+            hideSpeed: 250
+          },
+          onClose: function(notificationType) {
+            if(showCallbackNotifications){
+              alert(notificationType+ ' notification closed');
+            }
+            
+            if($('.notification-container').sudoNotify('getOption', 'autoHide') === true) {
+              clearInterval(countdown);
+              $('#countdown').html('0');
+              $('#closeButton').hide();
+            }
+          },
+          onShow: function(notificationType) {
+            if(showCallbackNotifications){
+              alert(notificationType+ ' notification showing');
+            }
+ 
+            if($('.notification-container').sudoNotify('getOption', 'autoHide') === true) {
+              var duration = $('.notification-container').sudoNotify('getOption', 'duration');
+              $('#countdown').html(duration);
+              $('#closeButton').show();
+              
+              duration = duration-1;
+              countdown = setInterval(function(){
+                if(duration === -1) {
+                  $('#countdown').html('0');
+                  $('#closeButton').hide();
+                  clearInterval(countdown);
+                }
+                else {
+                  $('#countdown').html(duration);
+                  duration--;
+                }
+              }, 1000);
+            }
+          }
+        });
 
-  var sudoNotify = $('.notification-container').sudoNotify({
-    autoHide: ($('#timeout').val() !== '0'),
-    showCloseButton: ($('#showCloseButton').val() === '1'),
-    duration: $('#timeout').val(), //seconds
-    position: $('#position').val(), //top or bottom
-    log: true,
-    opacity: 0.95,
-    defaultStyle: {
-      maxWidth: '1000px',
-      fontSize: '16px'
-    },
-    errorStyle: {
-      color: '#000000',
-      backgroundColor: '#FF9494'
-    },
-    warningStyle: {
-      color: '#000000',
-      backgroundColor: '#FFFF96'
-    },
-    successStyle: {
-      color: '#000000',
-      backgroundColor: '#B8FF6D'
-    },
-    animation: {
-      type: $('#animationType').val(), //fade, scroll-left, scroll-left-fade, scroll-right, scroll-right-fade, slide, slide-fade or none
-      showSpeed: 400,
-      hideSpeed: 250
-    },
-    onClose: function(notificationType) {
-      if(showCallbackNotifications){
-        alert(notificationType+ ' notification closed');
-      }
+        $('#successButton').click(function(){
+          $('.notification-container').sudoNotify('success', 'Great success!!!');
+        });
 
-      if(sudoNotify.getOption('autoHide') === true) {
-        clearInterval(countdown);
-        $('#countdown').html('0');
-        $('#closeButton').hide();
-      }
-    },
-    onShow: function(notificationType) {
-      if(showCallbackNotifications){
-        alert(notificationType+ ' notification showing');
-      }
+        $('#warningButton').click(function(){
+          $('.notification-container').sudoNotify('warning', 'Something\'s ratteling!');
+        });
 
-      if(sudoNotify.getOption('autoHide') === true) {
-        var duration = sudoNotify.getOption('duration');
-        $('#countdown').html(duration);
-        $('#closeButton').show();
+        $('#errorButton').click(function(){
+          $('.notification-container').sudoNotify('error', 'Stop right now!!!');
+        });
+        
+        $('#closeButton').click(function(){
+          $('.notification-container').sudoNotify('close');
+        });
 
-        duration = duration-1;
-        countdown = setInterval(function(){
-          if(duration === -1) {
-            $('#countdown').html('0');
-            $('#closeButton').hide();
-            clearInterval(countdown);
+        $('#animationType').change(function() {
+          $('.notification-container').sudoNotify('setOption', 'animation', {
+            type: $(this).val(),
+          });
+        });
+        
+        $('#timeout').change(function() {
+          if($(this).val() === '0') {
+            $('.notification-container').sudoNotify('setOption', 'autoHide', false);
           }
           else {
-            $('#countdown').html(duration);
-            duration--;
+            $('.notification-container').sudoNotify('setOption', 'autoHide', true);
+            $('.notification-container').sudoNotify('setOption', 'duration', $(this).val());
           }
-        }, 1000);
-      }
-    }
-  });
+        });
+        
+        $('#showCallbackNotifications').change(function() {
+          showCallbackNotifications = ($(this).val() === '1');
+        });
+        
+        $('#position').change(function() {
+          $('.notification-container').sudoNotify('setOption', 'position', $(this).val());
+        });
+        
+        $('#showCloseButton').change(function() {
+          $('.notification-container').sudoNotify('setOption', 'showCloseButton', ($(this).val() ==='1'));
+        });
+        
+        //Notifications within container
+        $('.notification-container-2').sudoNotify({
+          autoHide: false,
+          animation: {
+            type: 'scroll-left-fade'
+          },
+          onShow: function() {
+            $('#closeButton-2').show();
+          },
+          onClose: function() {
+            $('#closeButton-2').hide();
+          }
+        });
+        
+        $('#successButton-2').click(function(){
+          $('.notification-container-2').sudoNotify('success', 'Great success!!!');
+        });
 
-  $('#successButton').click(function(){
-    sudoNotify.success('Great success!!!');
-  });
+        $('#warningButton-2').click(function(){
+          $('.notification-container-2').sudoNotify('warning','Something\'s ratteling!');
+        });
 
-  $('#warningButton').click(function(){
-    sudoNotify.warning('Somthing\'s ratteling!');
-  });
-
-  $('#errorButton').click(function(){
-    sudoNotify.error('Stop right now!!!');
-  });
-
-  $('#closeButton').click(function(){
-    sudoNotify.close();
-  });
-
-  $('#animationType').change(function() {
-    sudoNotify.setOption('animation', {
-      type: $(this).val(),
-      showSpeed: $.fn.sudoNotify.defaults.animation.showSpeed,
-      hideSpeed: $.fn.sudoNotify.defaults.animation.hideSpeed
-    });
-  });
-
-  $('#timeout').change(function() {
-    if($(this).val() === '0') {
-      sudoNotify.setOption('autoHide', false);
-    }
-    else {
-      sudoNotify.setOption('autoHide', true);
-      sudoNotify.setOption('duration', $(this).val());
-    }
-  });
-
-  $('#showCallbackNotifications').change(function() {
-    showCallbackNotifications = ($(this).val() === '1');
-  });
-
-  $('#position').change(function() {
-    sudoNotify.setOption('position', $(this).val());
-  });
-
-  $('#showCloseButton').change(function() {
-    sudoNotify.setOption('showCloseButton', ($(this).val() ==='1'));
-  });
-
-  //Notifications within container
-  var sudoNotify2 = $('.notification-container-2').sudoNotify({
-    autoHide: false,
-    verticalMargin: '31px',
-    animation: {
-      type: 'scroll-left-fade'
-    },
-    onShow: function() {
-      $('#closeButton-2').show();
-    },
-    onClose: function() {
-      $('#closeButton-2').hide();
-    }
-  });
-
-  $('#successButton-2').click(function(){
-    sudoNotify2.success('Great success!!!');
-  });
-
-  $('#warningButton-2').click(function(){
-    sudoNotify2.warning('Somthing\'s ratteling!');
-  });
-
-  $('#errorButton-2').click(function(){
-    sudoNotify2.error('Stop right now!!!');
-  });
-
-  $('#closeButton-2').click(function(){
-    sudoNotify2.close();
-  });
-});
+        $('#errorButton-2').click(function(){
+          $('.notification-container-2').sudoNotify('error', 'Stop right now!!!');
+        });
+        
+        $('#closeButton-2').click(function(){
+          $('.notification-container-2').sudoNotify('close');
+        });
+      });
